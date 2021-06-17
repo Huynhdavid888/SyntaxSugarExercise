1298c3f994401241ca8fcf2fc355dba4a7e3ceb5
Huynhdavid888 <huynhdavid1245@gmail.com>
2021-06-17 08:47:00 -0700

    initial commit

---
+++ SyntaxSugarExercise/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace SyntaxSugarExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var answer = 4;
+          var response = (answer < 9) ? "Is less than 9" : "Greater than or equal to nine";
+        }
+    }
+}
